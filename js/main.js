d2d44c0308618bb58398f4fefd211efbba5c63dd
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-04-02 17:52:21 +0200

    Shortened script, added functionality for flexbox screenshots in markdown

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ const topOffsetSmall = 48;
 // Elements
 const navBar = document.getElementById('navbar');
 
-function handleMenuClick(elem) {
+window.handleMenuClick = function handleMenuClick(elem) {
   const targetElem = document.getElementById(elem.dataset.linkTo);
   window.scrollTo({top: targetElem.offsetTop - topOffsetSmall, behavior: 'smooth'});
 
@@ -22,33 +22,29 @@ function handleMenuClick(elem) {
   }
 }
 
-function openCV() {
-  if (confirm("Open English version?")) {
-    fetch(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265 - English.pdf`).then(response => {
-      response.blob().then( blob => {
-        const fileURL = URL.createObjectURL(blob);
-        window.open(fileURL);
-      });
+function getAndViewBlob(path) {
+  fetch(path).then(response => {
+    response.blob().then(blob => {
+      const fileURL = URL.createObjectURL(blob);
+      window.open(fileURL);
     });
+  });
+}
+
+window.openCV = function openCV() {
+  if (confirm("Open English version?")) { getAndViewBlob(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265 - English.pdf`);
   } else {
-    if (confirm("Open Dutch version?")) {
-      fetch(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265.pdf`).then(response => {
-        response.blob().then( blob => {
-          const fileURL = URL.createObjectURL(blob);
-          window.open(fileURL);
-        });
-      });
-    }
+    if (confirm("Open Dutch version?")) { getAndViewBlob(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265.pdf`); }
   }
 }
 
-function onLogoClick() {
+window.onLogoClick = function onLogoClick() {
   window.history.pushState(null, null, window.location.origin);
   closeDialog();
   scrollToTop();
 }
 
-function scrollToTop() {
+window.scrollToTop = function scrollToTop() {
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
@@ -68,7 +64,7 @@ function collapseNavBar() {
   }
 }
 
-function showMenu() {
+window.showMenu = function showMenu() {
   navBar.classList.toggle('shown');
   if (!navBar.classList.contains('shown')) {
     window.onscroll();
@@ -85,17 +81,19 @@ function hideLoader()  {
 
 function buildDialogContent (data) {
   let doc = document.createRange().createContextualFragment(data.toString());
-  doc.querySelectorAll('[alt="icon"]').forEach(e => { e.classList.add(e.getAttribute('alt')); });
+  doc.querySelectorAll('[alt]:not([alt=""])').forEach(e => { e.classList.add(e.getAttribute('alt').split(' ')[0]); });
   document.getElementById('dialog-content').innerHTML = '';
   document.getElementById('dialog-content').appendChild(doc);
   document.querySelectorAll('.dialog__content-wrapper')[0].scrollTop = 0;
+  console.log(document.querySelectorAll('#dialog-content img.flex').forEach( e => { e.parentElement.classList.add('flex'); }));
   hljs.highlightAll();
   openDialog();
+  collapseNavBar();
 }
 
 function getDialogContent(projectName) {
   showLoader();
-  fetch(`/markdown/${projectName}.md`).then(response => response.text() ).then(data => {
+  fetch(`/markdown/${projectName}.md`).then(response => response.text()).then(data => {
         data = marked(data);
         if (!data.toString().includes('<!doctype html>')) {
           buildDialogContent(data);
@@ -111,7 +109,7 @@ function openDialog() {
   document.getElementById('dialog').classList.add('active');
 }
 
-function closeDialog() {
+window.closeDialog = function closeDialog() {
   if (window.location.pathname !== '/') { window.history.pushState(null, null, window.location.origin); }
   document.body.classList.remove('scroll_disabled');
   document.getElementById('dialog').classList.remove('active');
@@ -129,7 +127,7 @@ function openDialogFromPathname(pathname) {
   }
 }
 
-function onProjectClick(projectName) {
+window.onProjectClick = function onProjectClick(projectName) {
     getDialogContent(projectName);
     if (!window.location.pathname.includes(projectName)) { window.history.pushState(null, projectName, '/' + projectName); }
 }
@@ -149,11 +147,3 @@ function init() {
 }
 
 init();
-
-window.handleMenuClick = handleMenuClick;
-window.openCV = openCV;
-window.onLogoClick = onLogoClick;
-window.scrollToTop = scrollToTop;
-window.showMenu = showMenu;
-window.closeDialog = closeDialog;
-window.onProjectClick = onProjectClick;
