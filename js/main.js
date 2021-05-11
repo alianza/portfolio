0744441026c4bc925d29d22b1163809f9292b474
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-05-11 23:56:30 +0200

    Moved a bunch of stuff, added scape key listener of closing dialog

--- js/main.js
+++ js/main.js
@@ -35,6 +35,21 @@ function init() {
 
   onScroll();
   onResize();
+
+  document.onkeydown = escapeKeyListener;
+}
+
+function escapeKeyListener(evt) {
+  evt = evt || window.event;
+  let isEscape;
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+    isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape) {
+    closeDialog()
+  }
 }
 
 function getAndViewBlob(path) {
