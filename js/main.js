02ed2ea13b9a60f7d65cfd7a05a82e34d7550c01
Alianza <janwillemvanbremen@live.nl>
2021-09-23 16:08:40 +0200

    Added load more projects button

--- js/main.js
+++ js/main.js
@@ -37,13 +37,13 @@ function init() {
 
   document.onkeydown = escapeKeyListener;
 
-  Object.entries(projects).forEach(([name, project]) => { // Iterate through projects and append to dom
+  Object.entries(projects).forEach(([name, project], index) => { // Iterate through projects and append to dom
     // noinspection HtmlDeprecatedAttribute
     document.querySelector('#experiences .wrapper').insertAdjacentHTML('beforeend',
-  `<div class="col clickable" onclick="onProjectClick(this.dataset.name)" data-name="${name}" data-team="${project.team}" data-tech="${project.tech}">
+  `<div class="col clickable ${index > 5 ? 'hidden' : ''}" onclick="onProjectClick(this.dataset.name)" data-name="${name}" data-team="${project.team}" data-tech="${project.tech}">
           <img class="img" alt="${name} project" src="../projects/${name}/${name}.webp" onerror="this.src='../tile.webp'"/>
           <h3>${project.title}</h3>
-        </div>`)
+        </div>`);
   });
 }
 
