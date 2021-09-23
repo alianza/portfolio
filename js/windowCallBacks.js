fd02f21bd7cc1bb57429cd70eb2469454ec5f7e8
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-09-23 03:33:21 +0200

    Added close mobile menu on resize

--- js/windowCallBacks.js
+++ js/windowCallBacks.js
@@ -1,5 +1,6 @@
 import { largeWidth, logo, mediumWidth, navBar, smallWidth, topOffsetBig } from "./constants";
 import { collapseNavBar, expandNavbar } from "./navBar";
+import * as constants from "./constants";
 
 function onScroll() {
     if (window.scrollY >= topOffsetBig) {
@@ -20,6 +21,7 @@ function onResize() {
         logo.innerText = "J.W. van Bremen";
     } else if (window.innerWidth >= smallWidth) {
         logo.innerText = "J.W.";
+        constants.navBar.classList.remove('open');
     }
 }
 
