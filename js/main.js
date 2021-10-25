79e9e30f2bc4743cc0339cbf44364611a6ed6a5c
Alianza <janwillemvanbremen@live.nl>
2021-10-26 01:51:03 +0200

    Refactored js, redid eventlisteners

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 import 'highlight.js/styles/xcode.css';
-import Accordion from './accordion';
-import { collapseNavBar } from "./navBar";
-import { hideLoader, showLoader } from "./loader";
-import { escapeKeyListener } from "./escapeKeyListener";
+import Accordion from './lib/accordion';
+import { collapseNavBar } from "./lib/navBar";
+import { hideLoader, showLoader } from "./lib/loader";
+import { escapeKeyListener } from "./lib/escapeKeyListener";
 import hljs from 'highlight.js/lib/core.js';
 import marked from 'marked';
 
 import * as constants from "./constants";
-import { calculateYearsSinceDate } from "./calculateYearsSinceDate";
-import { onResize, onScroll } from "./windowCallBacks";
-import { getAndViewBlob } from "./blob";
+import { calculateYearsSinceDate } from "./lib/calculateYearsSinceDate";
+import { onResize, onScroll } from "./lib/windowCallBacks";
+import { getAndViewBlob } from "./lib/blob";
 import projects from "../static/projects/projects.json"
 
 import javascript from 'highlight.js/lib/languages/javascript';
 import kotlin from 'highlight.js/lib/languages/kotlin';
-
-// Init
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('kotlin', kotlin);
 
 function init() {
   // document.getElementById('vid').playbackRate = .5; // Slow motion main video
@@ -25,17 +21,14 @@ function init() {
   document.getElementById('age').innerHTML = calculateYearsSinceDate(new Date('10-10-1998'));
   document.getElementById('years').innerHTML = calculateYearsSinceDate(new Date('1-7-2011'));
 
-  window.onscroll = onScroll;
-  window.onresize = onResize;
+  window.addEventListener('scroll', () => onScroll() );
+  window.addEventListener('resize', () => onResize() );
+  window.addEventListener('keydown', e => escapeKeyListener(e) );
+  window.addEventListener('popstate', e => openDialogFromPathname(e.path[0].location.pathname))
 
   onScroll();
   onResize();
-
   openDialogFromPathname(window.location.pathname);
-
-  window.onpopstate = event => { openDialogFromPathname(event.path[0].location.pathname) };
-
-  document.onkeydown = escapeKeyListener;
 
   Object.entries(projects).forEach(([name, project], index) => { // Iterate through projects and append to dom
     // noinspection HtmlDeprecatedAttribute
@@ -45,13 +38,16 @@ function init() {
           <h3>${project.title}</h3>
         </div>`);
   });
+
+  hljs.registerLanguage('javascript', javascript);
+  hljs.registerLanguage('kotlin', kotlin);
 }
 
 function buildDialogContent (data) {
   const doc = document.createRange().createContextualFragment(data.toString()); // Create HTML fragment from HTML string
-  doc.querySelectorAll('[alt]:not([alt=""])').forEach(e => { e.classList.add(e.getAttribute('alt').split(' ')[0]); }); // set classnames from first alt attribute value
-  doc.querySelectorAll('img.flex').forEach( e => { e.parentElement.classList.add('flex'); }); // Set flex attribute for flex images parent
-  doc.querySelectorAll('details').forEach((e) => { new Accordion(e); }); // Set Accordion animation for all details tags
+  doc.querySelectorAll('[alt]:not([alt=""])').forEach(e => { e.classList.add(e.getAttribute('alt').split(' ')[0]) }); // set classnames from first alt attribute value
+  doc.querySelectorAll('img.flex').forEach( e => { e.parentElement.classList.add('flex') }); // Set flex attribute for flex images parent
+  doc.querySelectorAll('details').forEach((e) => { new Accordion(e) }); // Set Accordion animation for all details tags
   doc.querySelectorAll('a').forEach((e) => { e.setAttribute('target', '_blank') }); // Open all links in new tabs
   constants.dialogContent.innerHTML = ''; // Clear dialog
   constants.dialogContent.appendChild(doc); // Fill dialog with data
@@ -89,7 +85,7 @@ window.openCV = () => { // Ask for language preference and open CV pdf blob
   if (confirm("Open English version?")) {
     getAndViewBlob(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265 - English.pdf`);
   } else if (confirm("Open Dutch version?")) {
-      getAndViewBlob(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265.pdf`);
+    getAndViewBlob(`/cv/Curriculum Vitae Jan-Willem van Bremen 500779265.pdf`);
   }
 }
 
@@ -100,28 +96,24 @@ window.onLogoClick = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-window.handleMenuClick = (elem)=> {
+window.handleMenuClick = (elem) => {
   const targetElem = document.getElementById(elem.dataset.linkTo);
   window.scrollTo({top: targetElem.offsetTop - constants.topOffsetSmall, behavior: 'smooth'});
 
-  if (constants.dialog.classList.contains('active')) {
-    closeDialog();
-  }
+  if (constants.dialog.classList.contains('active')) { closeDialog() }
 }
 
-window.onMenuButtonClick = () => {
-  constants.navBar.classList.toggle('open');
-}
+window.onMenuButtonClick = () => { constants.navBar.classList.toggle('open') }
 
 window.closeDialog = () => {
-  if (window.location.pathname !== '/') { window.history.pushState(null, null, window.location.origin); }
+  if (window.location.pathname !== '/') { window.history.pushState(null, null, window.location.origin) }
   document.body.classList.remove('scroll_disabled');
   constants.dialog.classList.remove('active');
 }
 
 window.onProjectClick = (projectName) => {
   getDialogContent(projectName);
-  if (!window.location.pathname.includes(projectName)) { window.history.pushState(null, projectName, '/' + projectName); }
+  if (!window.location.pathname.includes(projectName)) { window.history.pushState(null, projectName, '/' + projectName) }
 }
 
 init();
